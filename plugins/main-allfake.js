6b99dea367d9ebc1a37db9a902a04ab924556703
Angelithoxz <ryuseiclubxyz@gmail.com>
2025-07-14 22:13:22 -0500

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -27,17 +27,17 @@ return res.data
 console.log(`Error : ${e}`)
 }}
   
-global.creador = 'wa.me/5212202410659'
+global.creador = 'wa.me/51901930696'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.namechannel = '=͟͟͞❀ 𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★'
-global.namechannel2 = '=͟͟͞❀ 𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★'
-global.namegrupo = 'ᰔᩚ ᥡᥙkі sᥙ᥆ᥙ • ᥆𝖿іᥴіᥲᥣ ❀'
-global.namecomu = 'ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥴ᥆mᥙᥒі𝗍ᥡ ❀'
-global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
+global.namechannel = '=蜔蜔蜑鉂� 饾悕饾悽饾惂饾惃 饾悕饾悮饾悿饾悮饾惂饾惃 - 饾悅饾悺饾悮饾惂饾惂饾悶饾惀 鈴ね熗熗炌炩槄'
+global.namechannel2 = '=蜔蜔蜑鉂� 饾悆饾悶饾惎 饾悁饾惂饾悹饾悶饾惀 - 饾悅饾悺饾悮饾惂饾惂饾悶饾惀 鈴ね熗熗炌炩槄'
+global.namegrupo = '岚斸 饾惂饾悽饾惂饾惃 饾惂饾悮饾悿饾悮饾惂饾惃 鈥� 幞嗮潠垦栣ゴ褨幞册ィ 鉂�'
+global.namecomu = '岚斸 饾惀饾惃饾惀饾悽 饾悳饾惀饾惍饾悰 鈥� 幞瘁m幞欋褨饾棈幞� 鉂�'
+global.listo = '鉂� *Aqu铆 tienes 喔區鈥粚鈥喔�*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
 
-global.canalIdM = ["120363416409380841@newsletter", "120363416409380841@newsletter"]
-global.canalNombreM = ["ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥙ⍴ძᥲ𝗍ᥱs ❀", "ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥲ᥎іs᥆s ❀"]
+global.canalIdM = ["120363374826926142@newsletter", "120363338297109389@newsletter"]
+global.canalNombreM = ["岚斸 饾悕饾悽饾惂饾惃 饾悕饾悮饾悿饾悮饾惂饾惃 饾悗饾悷饾悽饾悳饾悽饾悮饾惀 饾悅饾悺饾悮饾惂饾惂饾悶饾惀鉂�", "岚斸 饾悆饾悶饾惎 饾悁饾惂饾悹饾悶饾惀"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
@@ -45,32 +45,32 @@ global.locale = 'es'
 global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
 global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
 global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
+global.a帽o = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
-global.rwait = '🕒'
-global.done = '✅'
-global.error = '✖️'
-global.msm = '⚠︎'
+global.rwait = '馃晵'
+global.done = '鉁�'
+global.error = '鉁栵笍'
+global.msm = '鈿狅笌'
 
-global.emoji = '❀'
-global.emoji2 = '✧'
-global.emoji3 = '✦'
-global.emoji4 = '❍'
-global.emoji5 = '✰'
+global.emoji = '鉂�'
+global.emoji2 = '鉁�'
+global.emoji3 = '鉁�'
+global.emoji4 = '鉂�'
+global.emoji5 = '鉁�'
 global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
 
-global.wait = '❍ Espera un momento, soy lenta...';
-global.waitt = '❍ Espera un momento, soy lenta...';
-global.waittt = '❍ Espera un momento, soy lenta...';
-global.waitttt = '❍ Espera un momento, soy lenta...';
+global.wait = '鉂� Espera un momento, soy lenta...';
+global.waitt = '鉂� Espera un momento, soy lenta...';
+global.waittt = '鉂� Espera un momento, soy lenta...';
+global.waitttt = '鉂� Espera un momento, soy lenta...';
 
-var canal = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'  
-var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-var git = 'https://github.com/The-King-Destroy'
-var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
-let correo = 'thekingdestroy507@gmail.com'
-global.redes = [canal, comunidad, git, github, correo].getRandom()
+var canal = 'https://whatsapp.com/channel/0029VapUpsT9mrGcypZy141s'  
+var comunidad = 'https://chat.whatsapp.com/HeKcGyv2idq0tMFRFGN6qL?mode=ac_t'
+var web = 'https://github.com/The-King-Destroy'
+var web2 = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
+let web3 = 'thekingdestroy507@gmail.com'
+global.redes = [canal, comunidad, web, web2, web3].getRandom()
 
 let category = "imagen"
 const db = './src/database/db.json'
@@ -81,16 +81,16 @@ const response = await fetch(randomlink)
 const rimg = await response.buffer()
 global.icons = rimg
 
-var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
+var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 1: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 2: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 3: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寗'; break; case 4: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寗'; break; case 5: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寗'; break; case 6: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寗'; break; case 7: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寘'; break; case 8: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寗'; break; case 9: hour = 'L瑟纱岽呩磤 M岽�纱虄岽�纱岽� 馃寗'; break; case 10: hour = 'L瑟纱岽呩磸 D瑟岽� 馃尋'; break; case 11: hour = 'L瑟纱岽呩磸 D瑟岽� 馃尋'; break; case 12: hour = 'L瑟纱岽呩磸 D瑟岽� 馃尋'; break; case 13: hour = 'L瑟纱岽呩磸 D瑟岽� 馃尋'; break; case 14: hour = 'L瑟纱岽呩磤 T岽�蕗岽呩磭 馃寙'; break; case 15: hour = 'L瑟纱岽呩磤 T岽�蕗岽呩磭 馃寙'; break; case 16: hour = 'L瑟纱岽呩磤 T岽�蕗岽呩磭 馃寙'; break; case 17: hour = 'L瑟纱岽呩磤 T岽�蕗岽呩磭 馃寙'; break; case 18: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 19: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 20: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 21: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 22: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break; case 23: hour = 'L瑟纱岽呩磤 N岽忈磩蕼岽� 馃寖'; break;}
 global.saludo = hour;
 
-global.nombre = m.pushName || 'Anónimo'
+global.nombre = m.pushName || 'An贸nimo'
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
-global.packsticker = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\nᰔᩚ Usuario: ${nombre}\n❀ Bot: ${botname}\n✦ Fecha: ${fecha}\nⴵ Hora: ${tiempo}`;
-global.packsticker2 = `\n°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n\n${dev}`
+global.packsticker = `掳.鈳儤潭鈳糕幆艽鈳夺碁酄澩団幆鈨樚垛幆谈鈳粹幆潭岢炧牆蛧鈳儤潭鈳�.掳\n岚斸 Usuario: ${nombre}\n鉂� Bot: ${botname}\n鉁� Fecha: ${fecha}\n獯� Hora: ${tiempo}`;
+global.packsticker2 = `\n掳.鈳儤潭鈳糕幆艽鈳夺碁酄澩団幆鈨樚垛幆谈鈳粹幆潭岢炧牆蛧鈳儤潭鈳�.掳\n\n${dev}`
   
 global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
 
@@ -98,7 +98,7 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [
-'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1749731657991.jpeg',
+'https://qu.ax/zzrcJ.jpg',
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
