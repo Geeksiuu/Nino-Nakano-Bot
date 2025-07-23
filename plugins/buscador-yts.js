e92380c9ba7904a030dda4f5e9542c4fbb38c983
Angelithoxz <ryuseiclubxyz@gmail.com>
2025-07-22 19:38:36 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,31 +1,37 @@
-import yts from 'yt-search'
+import yts from 'yt-search';
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingresa una busqueda de Youtube.`, m)
+  if (!text) {
+    return conn.reply(m.chat, `📺✨ *Nyaa~* porfis, escribe algo para buscar en *YouTube* 💬`, m);
+  }
 
-conn.reply(m.chat, wait, m)
+  await conn.reply(m.chat, '⏳ *Buscando tus videítos kawaii... espera un momento* 💖', m);
 
-let results = await yts(text)
-let tes = results.all
-let teks = results.all.map(v => {
-switch (v.type) {
-case 'video': return `「✦」Resultados de la búsqueda para *<${text}>*
+  let results = await yts(text);
+  let tes = results.all;
 
-> ☁️ Título » *${v.title}*
-> 🍬 Canal » *${v.author.name}*
-> 🕝 Duración » *${v.timestamp}*
-> 📆 Subido » *${v.ago}*
-> 👀 Vistas » *${v.views}*
-> 🔗 Enlace » ${v.url}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+  let teks = results.all.map(v => {
+    switch (v.type) {
+      case 'video':
+        return `╭──────⊹꙳✧༚
+🍓 *Título:* ${v.title}
+🎀 *Canal:* ${v.author.name}
+⏱ *Duración:* ${v.timestamp}
+📆 *Subido:* ${v.ago}
+👁️ *Vistas:* ${v.views}
+🔗 *Link:* ${v.url}
+╰───────────────✦`;
+    }
+  }).filter(v => v).join('\n\n');
 
-conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
+  await conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', `🌸 *Resultados de tu búsqueda:* “${text}”\n\n${teks}`, fkontak, m);
+};
 
-}
-handler.help = ['ytsearch']
-handler.tags = ['buscador']
-handler.command = ['ytbuscar', 'ytsearch', 'yts']
-handler.register = true
-handler.coin = 1
+handler.help = ['ytsearch'];
+handler.tags = ['buscador'];
+handler.command = ['ytbuscar', 'ytsearch', 'yts'];
+handler.register = true;
+handler.coin = 1;
 
-export default handler
+export default handler;
